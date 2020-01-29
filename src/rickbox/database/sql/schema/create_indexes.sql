b9a48099f471e590227bb65ab7f48d7556814a7e
Alain Dupuis <adupuis2@unb.ca>
2020-01-29 11:33:11 -0400

    Initial stored procedure creation

---
+++ src/rickbox/database/sql/schema/create_indexes.sql
@@ -0,0 +1,4 @@
+ CREATE INDEX `idx_files_folder_id`           ON `files` (`folder_id`);
+ CREATE INDEX `idx_files_owner_id`            ON `files` (`owner_id`);
+ CREATE INDEX `idx_folders_owner_id`          ON `folders` (`owner_id`);
+ CREATE INDEX `idx_users_root_folder_id`      ON `users` (`root_folder_id`);
