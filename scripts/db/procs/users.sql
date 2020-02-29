b913c13dbbab8c8a7eb0f61df2f81b55645b2d48
Alain Dupuis <adupuis2@unb.ca>
2020-02-29 13:45:00 -0400

    Simplified schema (and therefore stored procs)

--- scripts/db/procs/users.sql
+++ scripts/db/procs/users.sql
@@ -1,27 +1,27 @@
-DELIMITER //
-
 DROP PROCEDURE IF EXISTS add_user;
+DROP PROCEDURE IF EXISTS get_user_by_id;
+DROP PROCEDURE IF EXISTS get_user_by_username;
 
+DELIMITER //
 CREATE PROCEDURE add_user(
-    IN in_uuid VARCHAR(36),
-    IN in_login_name VARCHAR(255),
-    IN in_root_folder_id VARCHAR(36)
+    IN in_username VARCHAR(255),
+    IN in_name VARCHAR(255),
+    IN in_email VARCHAR(255)
+
 )
 BEGIN
-    INSERT INTO users (login_name, root_folder_id)
-    VALUES (in_login_name, in_root_folder_id);
-END//
+    INSERT INTO users (username, name, email)
+    VALUES (in_username, in_name, in_email);
+END //
 
-DROP PROCEDURE IF EXISTS get_user_by_id;
-CREATE PROCEDURE get_user_by_id(IN in_id VARCHAR(36))
+CREATE PROCEDURE get_user_by_id(IN in_id INT UNSIGNED)
 BEGIN
-    SELECT * FROM users WHERE uuid = in_id;
-end //
+    SELECT * FROM users WHERE id = in_id;
+END //
 
-DROP PROCEDURE IF EXISTS delete_user;
-CREATE PROCEDURE delete_user(IN in_user_id VARCHAR(36))
+CREATE PROCEDURE get_user_by_username(IN in_username VARCHAR(255))
 BEGIN
-    UPDATE users SET active = FALSE WHERE uuid = in_user_id;
-end //
+    SELECT * FROM users WHERE username = in_username;
+END //
 
 DELIMITER ;
--- scripts/db/schema/fks.sql
+++ scripts/db/schema/fks.sql
@@ -1,23 +1,4 @@
 ALTER TABLE files
-  ADD FOREIGN KEY (folder_id)
-  REFERENCES folders (uuid)
-  ON DELETE CASCADE;
-
-ALTER TABLE files
   ADD FOREIGN KEY (owner_id)
-  REFERENCES users (uuid)
+  REFERENCES users (id)
   ON DELETE CASCADE;
-
-ALTER TABLE folders
-  ADD FOREIGN KEY (owner_id)
-  REFERENCES users (uuid)
-  ON DELETE CASCADE;
-
-ALTER TABLE folders
-  ADD FOREIGN KEY (parent_folder_id)
-  REFERENCES folders (uuid)
-  ON DELETE CASCADE;
-
-ALTER TABLE users
-  ADD FOREIGN KEY (root_folder_id)
-  REFERENCES folders (uuid);
